d96f6fa46d60b7cb75679e87c7c230c8beb57fcc
Apeksha <poudel.apeksha65@gmail.com>
2024-08-16 22:28:20 +0545

    Apeksha:8/16

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ const toggleSwitch = document.getElementById('mode-toggle');
 
 toggleSwitch.addEventListener('change', function() {
     document.body.classList.toggle('dark-mode', this.checked);
+    document.querySelector('.section').classList.toggle('dark-mode', this.checked);
+    document.querySelector('footer').classList.toggle('dark-mode', this.checked);
+    document.querySelector('.big').classList.toggle('dark-mode', this.checked);
 });
